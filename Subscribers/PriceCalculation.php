907910b5fae55185152886b1e1d7291016af3179
Julian Krzefski <krzefski@ksk-agentur.de>
2017-09-04 10:32:03 +0200

    Added support for product exports

--- Subscribers/PriceCalculation.php
+++ Subscribers/PriceCalculation.php
@@ -6,12 +6,14 @@ use Doctrine\ORM\EntityRepository;
 use Enlight\Event\SubscriberInterface;
 use Enlight_Event_EventArgs;
 use KskCustomPriceCalc\Models\CurrencySettings;
+use sExport;
 use Shopware\Bundle\StoreFrontBundle\Gateway\DBAL\Hydrator\TaxHydrator;
 use Shopware\Bundle\StoreFrontBundle\Service\Core\ContextService;
 use Shopware\Bundle\StoreFrontBundle\Struct\Customer\Group;
-use Shopware\Bundle\StoreFrontBundle\Struct\ProductContextInterface;
+use Shopware\Bundle\StoreFrontBundle\Struct\ShopContextInterface;
 use Shopware\Bundle\StoreFrontBundle\Struct\Tax;
 use Shopware\Components\Model\ModelManager;
+use Shopware\Models\Shop\DetachedShop;
 use Shopware\Models\Tax\Tax as TaxModel;
 
 /**
@@ -88,7 +90,78 @@ class PriceCalculation implements SubscriberInterface
         return [
             'Shopware_StoreFrontBundle_PriceCalculationService_Filter_Price' => 'filterPrice',
             'Shopware_Modules_Basket_getPriceForUpdateArticle_FilterPrice' => 'onUpdatePrice',
+            'Shopware_Modules_Export_ExportResult_Filter_Fixed' => 'exportFilter',
         ];
+    }
+
+    /**
+     * @param Enlight_Event_EventArgs $args
+     * @return mixed
+     */
+    public function exportFilter(Enlight_Event_EventArgs $args)
+    {
+        /** @var sExport $export */
+        $export = $args->get('subject');
+        /** @var DetachedShop $shop */
+        $shop = $export->shop;
+        /** @var array $articles */
+        $articles = $args->getReturn();
+
+        $context = $this->contextService->createShopContext(
+            $shop->getId(), $shop->getCurrency()->getId(), $export->sCustomergroup['groupkey']
+        );
+
+        $customerGroup = $context->getCurrentCustomerGroup();
+
+        /** @var TaxModel $taxModel */
+        $taxModel = $this->modelManager->find(TaxModel::class, $export->sCustomergroup['tax']);
+        $tax = $this->taxHydrator->hydrate([
+            '__tax_id' => $taxModel->getId(),
+            '__tax_description' => $taxModel->getName(),
+            '__tax_tax' => $taxModel->getTax(),
+        ]);
+
+        /** @var array $article */
+        foreach ($articles as &$article) {
+            $article['netprice'] = $this->subtractTax($this->calculateExportPrices($article['netprice'], $tax, $context, $customerGroup), $tax);
+            $article['netprice_numeric'] = $this->subtractTax($this->calculateExportPrices($article['netprice_numeric'], $tax, $context, $customerGroup), $tax);
+            $article['price'] = $this->calculateExportPrices($article['price'], $tax, $context, $customerGroup);
+            $article['price_numeric'] = $this->calculateExportPrices($article['price_numeric'], $tax, $context, $customerGroup);
+            $article['netpseudoprice'] = $this->subtractTax($this->calculateExportPrices($article['netpseudoprice'], $tax, $context, $customerGroup), $tax);
+            $article['netpseudoprice_numeric'] = $this->subtractTax($this->calculateExportPrices($article['netpseudoprice_numeric'], $tax, $context, $customerGroup), $tax);
+            $article['referenceprice'] = $this->calculateExportPrices($article['referenceprice'], $tax, $context, $customerGroup);
+        }
+
+        return $articles;
+    }
+
+    /**
+     * @param float $price
+     * @param Tax $tax
+     * @return float
+     */
+    protected function subtractTax($price, Tax $tax)
+    {
+        $newPrice = $price / (1 + $tax->getTax() / 100);
+        return round($newPrice, 2, PHP_ROUND_HALF_UP);
+    }
+
+    /**
+     * @param float $price
+     * @param Tax $tax
+     * @param ShopContextInterface $context
+     * @param Group $customerGroup
+     * @return float
+     */
+    private function calculateExportPrices($price, Tax $tax, ShopContextInterface $context, Group $customerGroup)
+    {
+        $newPrice = $this->calculateCustomPrice($price, $tax, $context, $customerGroup);
+
+        if ($customerGroup->displayGrossPrices() && $newPrice != $price) {
+            $newPrice *= (1 + $tax->getTax() / 100);
+        }
+
+        return $newPrice;
     }
 
     /**
@@ -100,7 +173,7 @@ class PriceCalculation implements SubscriberInterface
         $price = $args->getReturn();
         /** @var Tax $tax */
         $tax = $args->get('tax');
-        /** @var ProductContextInterface $context */
+        /** @var ShopContextInterface $context */
         $context = $args->get('context');
         /** @var Group $customerGroup */
         $customerGroup = $args->get('customerGroup');
@@ -117,6 +190,7 @@ class PriceCalculation implements SubscriberInterface
         /** @var array $return */
         $return = $args->getReturn();
 
+        /** @var TaxModel $taxModel */
         $taxModel = $this->modelManager->getRepository(TaxModel::class)->findOneBy(['tax' => $return['tax']]);
         $tax = $this->taxHydrator->hydrate([
             '__tax_id' => $taxModel->getId(),
@@ -124,7 +198,7 @@ class PriceCalculation implements SubscriberInterface
             '__tax_tax' => $taxModel->getTax(),
         ]);
 
-        /** @var ProductContextInterface $context */
+        /** @var ShopContextInterface $context */
         $context = $this->contextService->getShopContext();
         $customerGroup = $context->getCurrentCustomerGroup();
 
@@ -138,11 +212,11 @@ class PriceCalculation implements SubscriberInterface
     /**
      * @param $price
      * @param Tax $tax
-     * @param ProductContextInterface $context
+     * @param ShopContextInterface $context
      * @param Group $customerGroup
      * @return float|int
      */
-    private function calculateCustomPrice($price, Tax $tax, ProductContextInterface $context, Group $customerGroup)
+    private function calculateCustomPrice($price, Tax $tax, ShopContextInterface $context, Group $customerGroup)
     {
         $currency = $context->getCurrency();
 
